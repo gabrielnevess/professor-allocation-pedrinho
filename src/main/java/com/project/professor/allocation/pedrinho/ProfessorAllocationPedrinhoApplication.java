340444a6a834d17421ff994f998e72c99109f45d
Gabriel Neves <gabriel-silva_2011@hotmail.com>
2024-02-24 15:11:43 -0300

    add project

---
+++ src/main/java/com/project/professor/allocation/pedrinho/ProfessorAllocationPedrinhoApplication.java
@@ -0,0 +1,13 @@
+package com.project.professor.allocation.pedrinho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorAllocationPedrinhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorAllocationPedrinhoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/professor/allocation/pedrinho/ProfessorAllocationPedrinhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.professor.allocation.pedrinho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessorAllocationPedrinhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
